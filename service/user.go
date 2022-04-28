ebdb1bfb9be00162c9696e6017b51d57aebdb41c
BFlameSwift <19373732@buaa.edu.cn>
2022-04-28 16:12:43 +0800

    chore: add some db opt

--- service/user.go
+++ service/user.go
@@ -30,3 +30,23 @@ func GetUserByID(ID uint64) (user database.User, notFound bool) {
 		return user, false
 	}
 }
+
+// SaveUser 根据信息保存用户
+func SaveUser(user *database.User) (err error) {
+	err = global.DB.Save(user).Error
+	return err
+}
+
+// DeleteUserByID 根据uid 删除用户
+func DeleteUserByID(ID uint64) (err error) {
+	if err = global.DB.Where("id = ?", ID).Delete(database.User{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+//// Query 查询用户对某问题的所有评测结果
+//func QueryUserResult(uid uint64, pid uint64) (results []database.Result) {
+//	global.DB.Where("user_id = ? AND problem_id = ?", uid, pid).Find(&results)
+//	return results
+//}
